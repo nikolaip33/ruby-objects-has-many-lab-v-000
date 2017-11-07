ac92c22f4872c9100ea1c07db14e6b216ece8fc3
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-07 16:26:23 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,26 +1,60 @@
+# class Author
+# 
+#   attr_accessor :name, :posts
+#   @@post_count = 0
+# 
+#   def initialize(name)
+#     @name = name
+#     @posts = []
+#   end
+# 
+#   def add_post(post)
+#     self.posts << post
+#     post.author = self
+#     @@post_count += 1
+#   end
+# 
+#   def add_post_by_title(title)
+#     post = Post.new(title)
+#     self.add_post(post)
+#   end
+# 
+#   ### class methods ###
+#   def self.post_count
+#     @@post_count
+#   end
+# end #class Author
+
+
 class Author
+    
+        attr_accessor :name, :posts, :post, :title
+    
+        @@post_count = 0
 
-  attr_accessor :name, :posts
-  @@post_count = 0
+        def initialize(name)
+            @name = name  
+            @posts = []   
+        end
+    
+        def posts
+            @posts
+        end
+    
+        def add_post(post)
+            @posts << post
+            post.author = self    
+            @@post_count +=1 
+        end
+      
+        def add_post_by_title(title)
+            post = Post.new(title)
+            add_post(post)
+        end
 
-  def initialize(name)
-    @name = name
-    @posts = []
-  end
-
-  def add_post(post)
-    @posts << post
-    post.author = self
-    @@post_count += 1
-  end
-
-  def add_post_by_title(title)
-    post = Post.new(title)
-    add_post(post)
-  end
-
-  ### class methods ###
-  def self.post_count
-    @@post_count
-  end
-end #class Author
+        #   binding.pry
+        def self.post_count
+            @@post_count
+        end
+    
+    end
