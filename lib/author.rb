f58dd57757d75ba779add63c3119b849852de423
Nikolai Pawlowski <nikolaip@gmail.com>
2017-11-07 16:26:35 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,24 +1,24 @@
 # class Author
-# 
+#
 #   attr_accessor :name, :posts
 #   @@post_count = 0
-# 
+#
 #   def initialize(name)
 #     @name = name
 #     @posts = []
 #   end
-# 
+#
 #   def add_post(post)
 #     self.posts << post
 #     post.author = self
 #     @@post_count += 1
 #   end
-# 
+#
 #   def add_post_by_title(title)
 #     post = Post.new(title)
 #     self.add_post(post)
 #   end
-# 
+#
 #   ### class methods ###
 #   def self.post_count
 #     @@post_count
@@ -27,26 +27,26 @@
 
 
 class Author
-    
+
         attr_accessor :name, :posts, :post, :title
-    
+
         @@post_count = 0
 
         def initialize(name)
-            @name = name  
-            @posts = []   
+            @name = name
+            @posts = []
         end
-    
+
         def posts
             @posts
         end
-    
+
         def add_post(post)
             @posts << post
-            post.author = self    
-            @@post_count +=1 
+            post.author = self
+            @@post_count +=1
         end
-      
+
         def add_post_by_title(title)
             post = Post.new(title)
             add_post(post)
@@ -56,5 +56,5 @@ class Author
         def self.post_count
             @@post_count
         end
-    
+
     end
